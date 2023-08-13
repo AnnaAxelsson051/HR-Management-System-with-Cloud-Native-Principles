96a11c19219ac260760c6f74dbdf9ca08e876513
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-13 20:21:23 +0200

    Add department service and employee service

---
+++ department-service/src/test/java/net/javamicro/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javamicro.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
