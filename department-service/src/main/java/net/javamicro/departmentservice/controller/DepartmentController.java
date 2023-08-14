ff24cc77d1b788dcdfc13f926033d7336ac41896
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-14 22:02:37 +0200

    Add departmentcontroller and saveDepartment method

---
+++ department-service/src/main/java/net/javamicro/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,22 @@
+package net.javamicro.departmentservice.controller;
+
+import lombok.AllArgsConstructor;
+import net.javamicro.departmentservice.dto.DepartmentDto;
+import net.javamicro.departmentservice.service.DepartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+    private DepartmentService departmentService;
+
+    //Saving departmentDto
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+}
