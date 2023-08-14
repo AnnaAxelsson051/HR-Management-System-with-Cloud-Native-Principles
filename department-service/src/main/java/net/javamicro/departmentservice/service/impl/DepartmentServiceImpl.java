809187cc480a1201639be4b42700c3c8695737da
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-14 21:35:37 +0200

    Convert department dto to dept jpa entity

--- department-service/src/main/java/net/javamicro/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/net/javamicro/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -2,6 +2,7 @@ package net.javamicro.departmentservice.service.impl;
 
 import lombok.AllArgsConstructor;
 import net.javamicro.departmentservice.dto.DepartmentDto;
+import net.javamicro.departmentservice.entity.Department;
 import net.javamicro.departmentservice.repository.DepartmentRepository;
 import net.javamicro.departmentservice.service.DepartmentService;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,22 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
-        return null;
+        //converting dept dto to dept jpa entity
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+        Department savedDepartment = departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = new DepartmentDto(
+                savedDepartment.getId(),
+                savedDepartment.getDepartmentName(),
+                savedDepartment.getDepartmentDescription(),
+                savedDepartment.getDepartmentCode()
+        );
+
+        return savedDepartmentDto;
     }
 }
