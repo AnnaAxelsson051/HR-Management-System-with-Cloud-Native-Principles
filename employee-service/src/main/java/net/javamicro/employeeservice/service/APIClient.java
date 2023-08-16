9ff8c9af2a319bffa7cde224bf90d9ee927688b0
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-16 16:25:50 +0200

    Create feign api client

--- employee-service/src/main/java/net/javamicro/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/net/javamicro/employeeservice/service/APIClient.java
@@ -1,10 +1,22 @@
 package net.javamicro.employeeservice.service;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import net.javaguides.employeeservice.dto.DepartmentDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-
-@FeignClient
+// Using Spring Cloud OpenFeign to create a client interface for
+// making HTTP requests to a service located at the url
+@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
-    //Getting department by code
+    @GetMapping("api/departments/{department-code}")
+    DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
+        }
 
-}
