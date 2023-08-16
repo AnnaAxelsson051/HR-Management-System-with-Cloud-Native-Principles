349e61d44f3d947a989a126ebb71a42f76fe111b
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-16 22:31:58 +0200

    Modify readme

--- README.md
+++ README.md
@@ -5,12 +5,17 @@
 
 A HR Management System built with Spring Boot, microservices and Spring Cloud.
 
-## Technologies used
+## Technologies and design
 
 - Spring Boot
+- Cloud Native 
+- Domain Driven Design
+- Microservices
+- Spring Cloud Open Feign for microservice communication
+- Eureka and Service discovery to manage and locate services
+- Loadbalancer to distribute traffic
+- Spring Cloud Gateway
 - MVC
 - RESTful web services
 - Spring Data Jpa
-- Spring Cloud Open Feign for microservice communication
-- Eureka and Service discovery to manage and locate services
 - MySQL
--- employee-service/src/main/java/net/javamicro/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/net/javamicro/employeeservice/service/APIClient.java
@@ -1,21 +1,19 @@
 package net.javamicro.employeeservice.service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import net.javamicro.employeeservice.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import net.javaguides.employeeservice.dto.DepartmentDto;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-// Using Spring Cloud OpenFeign to create a client interface for
-// making HTTP requests to a service located at the url
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+
+// Service that the feign client will communicate with
+// Eureka client will internally use load balancer to
+// distribute traffic
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
+    // retrieving department information based on a specified department code
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
         }
