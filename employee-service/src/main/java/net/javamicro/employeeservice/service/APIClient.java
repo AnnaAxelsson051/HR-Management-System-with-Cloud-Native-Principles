885f919f36ec6014f418f7381b44beeb1f3068b9
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-16 16:14:49 +0200

    Add APIClient, spring cloud open feign dependency and Feign Client

---
+++ employee-service/src/main/java/net/javamicro/employeeservice/service/APIClient.java
@@ -0,0 +1,10 @@
+package net.javamicro.employeeservice.service;
+import org.springframework.cloud.openfeign.FeignClient;
+
+
+@FeignClient
+public interface APIClient {
+
+    //Getting department by code
+
+}
