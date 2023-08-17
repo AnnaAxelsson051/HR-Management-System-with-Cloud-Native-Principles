8fa38bea8094b7466f19ca8e95e0937d40b9b00a
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-17 15:01:53 +0200

    Register API-gateway as Eureka client to Eureka server

--- employee-service/src/main/java/net/javamicro/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/javamicro/employeeservice/EmployeeServiceApplication.java
@@ -2,23 +2,13 @@ package net.javamicro.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
 public class EmployeeServiceApplication {
-
-	//Configure webclient as a spring bean
-/*
-	@Bean
-	public WebClient webClient(){
-		return WebClient.builder().build();
-	}*/
 
 
 	public static void main(String[] args) {
