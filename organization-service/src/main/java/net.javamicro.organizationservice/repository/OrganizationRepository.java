c901f865a4dc9026f230e25dedc2b41096c5fa62
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-19 20:47:39 +0200

    Add OrganizationDto and Organization repo

---
+++ organization-service/src/main/java/net.javamicro.organizationservice/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package net.javamicro.organizationservice.repository;
+
+import net.javamicro.organizationservice.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface OrganizationRepository extends JpaRepository<Organization, Long>{
+
+}
