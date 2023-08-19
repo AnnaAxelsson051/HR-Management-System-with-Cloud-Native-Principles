b9c889be107ab7300c1f4cb122308255c772d239
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-19 20:56:06 +0200

    Add organization mapper

---
+++ organization-service/src/main/java/net.javamicro.organizationservice/mapper/OrganizationMapper.java
@@ -0,0 +1,29 @@
+package net.javamicro.organizationservice.mapper;
+
+import net.javamicro.organizationservice.dto.OrganizationDto;
+import net.javamicro.organizationservice.entity.Organization;
+
+public class OrganizationMapper {
+
+    public static OrganizationDto mapToOrganizationDto(Organization organization){
+        OrganizationDto organizationDto = new OrganizationDto(
+                organization.getId(),
+                organization.getOrganizationName(),
+                organization.getOrganizationDescription(),
+                organization.getOrganizationCode(),
+                organization.getCreatedDate()
+        );
+        return organizationDto;
+    }
+
+    public static Organization mapToOrganization(OrganizationDto organizationDto){
+        Organization organization = new Organization(
+                organizationDto.getId(),
+                organizationDto.getOrganizationName(),
+                organizationDto.getOrganizationDescription(),
+                organizationDto.getOrganizationCode(),
+                organizationDto.getCreatedDate()
+        );
+        return organization;
+    }
+}
