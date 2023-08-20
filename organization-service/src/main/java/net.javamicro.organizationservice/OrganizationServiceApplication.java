9c997b00afdfadc5ace11e943b38c26529e2b851
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-20 20:04:22 +0200

    Register organisation service as eureka client with eureka server

--- organization-service/src/main/java/net.javamicro.organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/net.javamicro.organizationservice/OrganizationServiceApplication.java
@@ -2,8 +2,10 @@ package net.javamicro.organizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
