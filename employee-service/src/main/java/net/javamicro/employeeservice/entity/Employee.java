0c637111a097ee73a55a03714ede8f0a4dda00c0
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-20 19:15:22 +0200

    Refractor code and add organization code field

--- employee-service/src/main/java/net/javamicro/employeeservice/entity/Employee.java
+++ employee-service/src/main/java/net/javamicro/employeeservice/entity/Employee.java
@@ -1,10 +1,11 @@
 package net.javamicro.employeeservice.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Setter
 @Getter
@@ -13,11 +14,13 @@ import lombok.Setter;
 @Entity
 @Table(name = "employees")
 public class Employee {
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     @Column(nullable = false, unique = true)
     private String email;
     private String departmentCode;
+    private String organizationCode;
 }
--- employee-service/src/main/java/net/javamicro/employeeservice/mapper/EmployeeMapper.java
+++ employee-service/src/main/java/net/javamicro/employeeservice/mapper/EmployeeMapper.java
@@ -11,7 +11,8 @@ public class EmployeeMapper {
                 employee.getFirstName(),
                 employee.getLastName(),
                 employee.getEmail(),
-                employee.getDepartmentCode()
+                employee.getDepartmentCode(),
+                employee.getOrganizationCode()
         );
         return employeeDto;
     }
@@ -22,7 +23,8 @@ public class EmployeeMapper {
                 employeeDto.getFirstName(),
                 employeeDto.getLastName(),
                 employeeDto.getEmail(),
-                employeeDto.getDepartmentCode()
+                employeeDto.getDepartmentCode(),
+                employeeDto.getOrganizationCode()
         );
         return employee;
     }
