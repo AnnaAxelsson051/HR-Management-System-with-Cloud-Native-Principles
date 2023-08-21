06fdb5b39c419180d7fad1a436062f70b9685a85
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-21 15:00:18 +0200

    Add api call from frontend to API gateway

---
+++ react-frontend/src/service/EmployeeService.js
@@ -0,0 +1,15 @@
+//Rest API call to API-gateway
+import axios from 'axios';
+
+const EMPLOYEE_SERVICE_BASE_URL= "http://localhost:9191/api/employees";
+
+const EMPLOYEE_ID = 2;
+
+class EmployeeService {
+
+    getEmployee (){
+        axios.get(EMPLOYEE_SERVICE_BASE_URL + '/' + EMPLOYEE_ID);
+    }
+}
+
+export default new EmployeeService
