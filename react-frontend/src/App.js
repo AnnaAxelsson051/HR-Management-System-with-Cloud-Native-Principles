e4e974774667554e7f8e75d2dca1dda53e9bcefb
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-21 18:42:24 +0200

    Add rendering of employee data in fields

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+{/* <div className="App"> 
+To fill spaces on sides*/
+}
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
     <h1>HR Management System</h1>
-    {/*  <EmployeeComponent/> */}
+   
+    <EmployeeComponent/>
       </header>
     </div>
   );
--- react-frontend/src/component/EmployeeComponent.js
+++ react-frontend/src/component/EmployeeComponent.js
@@ -27,7 +27,20 @@ class EmployeeComponent extends Component {
     render() {
         return (
             <div>
-                
+                <div className='card col-md-6 offset-md-3'>
+                <h3 className='text-center card-header'>View Employee Details</h3>
+                <div className='card-body'>
+                    <div className='row'>
+                    <p><strong>Employee First Name: </strong>{this.state.employee.firstName}</p>
+                    </div>
+                    <div className='row'>
+                    <p><strong>Employee Last Name: </strong>{this.state.employee.lastName}</p>
+                    </div>
+                    <div className='row'>
+                    <p><strong>Employee Email: </strong>{this.state.employee.email}</p>
+                    </div>
+                </div>
+                </div>
             </div>
         );
     }
