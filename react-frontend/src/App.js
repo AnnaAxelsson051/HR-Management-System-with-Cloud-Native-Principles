b751ebaf0990daaf1f72185fb6437d2dac385498
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-21 19:18:04 +0200

    Design page to display employee, department and organization details

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import EmployeeComponent from './component/EmployeeComponent';
 
 {/* <div className="App"> 
-To fill spaces on sides*/
+To fill spaces on sides
+
+Göra header visible för svart bakgrund*/
 }
 function App() {
   return (
     <div className="container">
-      <header className="App-header">
+     {/*<header className="App-header">*/} 
     <h1>HR Management System</h1>
-   
     <EmployeeComponent/>
-      </header>
+      {/*</header>*/}
     </div>
   );
 }
